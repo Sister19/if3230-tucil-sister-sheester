32e6a27866b54f545c5b54070379a58983292caa
danielsalim <salimdaniel28@gmail.com>
2023-03-16 12:32:49 +0700

    add cuda parallel

--- src/cuda/cuda.cu
+++ src/cuda/cuda.cu
@@ -1,56 +1,96 @@
-#include <cuComplex.h>
+// Cuda Parallel 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <cuda_runtime.h>
+#include <cuComplex.h>
 
 #define MAX_N 512
+#define BLOCK_SIZE 16
 
 struct Matrix {
-    int    size;
+    int size;
     double mat[MAX_N][MAX_N];
 };
 
 struct FreqMatrix {
-    int    size;
+    int size;
     cuDoubleComplex mat[MAX_N][MAX_N];
 };
 
-cuDoubleComplex cuCexp(cuDoubleComplex x)
+__device__ cuDoubleComplex cuCexp(cuDoubleComplex x)
 {
- double factor = exp(x.x);
- return make_cuDoubleComplex(factor * cos(x.y), factor * sin(x.y));
+    double factor = exp(x.x);
+    return make_cuDoubleComplex(factor * cos(x.y), factor * sin(x.y));
 }
 
-void readMatrix(struct Matrix *m) {
+__global__ void dft_kernel(struct Matrix *mat, struct FreqMatrix *freq_domain)
+{
+  // Implement shared memory
+    __shared__ double shared_mat[BLOCK_SIZE][BLOCK_SIZE];
+
+    int k = blockIdx.x;
+    int l = threadIdx.x;
+
+    cuDoubleComplex element = make_cuDoubleComplex(0.0, 0.0);
+
+    for (int i = 0; i < mat->size; i += BLOCK_SIZE) {
+        for (int j = 0; j < mat->size; j += BLOCK_SIZE) {
+            // Load a block of input matrix into shared memory
+            shared_mat[l][k] = mat->mat[i + l][j + k];
+
+            __syncthreads();
+
+            for (int m = 0; m < BLOCK_SIZE; m++) {
+                for (int n = 0; n < BLOCK_SIZE; n++) {
+                    cuDoubleComplex arg      = make_cuDoubleComplex((i + m) * k / (double) mat->size + (j + n) * l / (double) mat->size, 0.0);
+                    cuDoubleComplex exponent = cuCexp(make_cuDoubleComplex(0.0, -2.0 * M_PI * arg.x));
+                    cuDoubleComplex value    = make_cuDoubleComplex(shared_mat[m][k], 0.0);
+                    element = cuCadd(element, cuCmul(value, exponent));
+                }
+            }
+
+            __syncthreads();
+        }
+    }
+
+    element = cuCdiv(element, make_cuDoubleComplex(mat->size*mat->size, 0.0));
+    freq_domain->mat[k][l] = element;
+}
+
+void readMatrix(struct Matrix *m)
+{
     scanf("%d", &(m->size));
     for (int i = 0; i < m->size; i++)
         for (int j = 0; j < m->size; j++)
             scanf("%lf", &(m->mat[i][j]));
 }
 
-cuDoubleComplex dft(struct Matrix *mat, int k, int l) {
-    cuDoubleComplex element = make_cuDoubleComplex(0.0, 0.0);
-    for (int m = 0; m < mat->size; m++) {
-        for (int n = 0; n < mat->size; n++) {
-            cuDoubleComplex arg      = make_cuDoubleComplex(k*m / (double) mat->size + l*n / (double) mat->size, 0.0);
-            cuDoubleComplex exponent = cuCexp(make_cuDoubleComplex(0.0, -2.0 * M_PI * arg.x));
-            cuDoubleComplex value = make_cuDoubleComplex(mat->mat[m][n], 0.0);
-            element = cuCadd(element, cuCmul(value, exponent));
-        }
-    }
-    element = cuCdiv(element, make_cuDoubleComplex(mat->size*mat->size, 0.0));
-    return element;
-}
-
-int main(void) {
-    struct Matrix     source;
+int main(void)
+{
+    struct Matrix source;
     struct FreqMatrix freq_domain;
     readMatrix(&source);
     freq_domain.size = source.size;
-    
-    for (int k = 0; k < source.size; k++)
-        for (int l = 0; l < source.size; l++)
-            freq_domain.mat[k][l] = dft(&source, k, l);
+
+    // Allocate device memory
+    struct Matrix *d_source;
+    cudaMalloc(&d_source, sizeof(struct Matrix));
+    cudaMemcpy(d_source, &source, sizeof(struct Matrix), cudaMemcpyHostToDevice);
+
+    struct FreqMatrix *d_freq_domain;
+    cudaMalloc(&d_freq_domain, sizeof(struct FreqMatrix));
+    cudaMemcpy(d_freq_domain, &freq_domain, sizeof(struct FreqMatrix), cudaMemcpyHostToDevice);
+
+    // Launch kernel
+    dft_kernel<<<source.size, source.size>>>(d_source, d_freq_domain);
+
+    // Copy results back to host
+    cudaMemcpy(&freq_domain, d_freq_domain, sizeof(struct FreqMatrix), cudaMemcpyDeviceToHost);
+
+    // Free device memory
+    cudaFree(d_source);
+    cudaFree(d_freq_domain);
 
     cuDoubleComplex sum = make_cuDoubleComplex(0.0, 0.0);
     for (int k = 0; k < source.size; k++) {
@@ -61,13 +101,8 @@ int main(void) {
         }
         printf("\n");
     }
-    sum = cuCdiv(sum, make_cuDoubleComplex(freq_domain.size, 0.0));
-    printf("Average : (%lf, %lf)", sum.x, sum.y);
-
     return 0;
-}
-
-
+};
 
 
 //alternatives
